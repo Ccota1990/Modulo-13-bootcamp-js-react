4f3f176971ccd15358763e31af2b1f6c0ddc77f3
Coral Cota Riera <ccota@intricom.es>
2024-03-11 12:23:22 +0100

    react js acabado

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,35 @@
+import React from "react";
+
 export const App = () => {
+  const [usuario, setUsuario] = React.useState({
+   nombre: "Manolo",
+   apellidos: "Torres"  
+  });
+  
+
   return (
     <>
-      <h1>Bootcamps JS - JavaScript React SandBox</h1>
+      <h1>Nombre: {usuario.nombre}</h1>
+      <input
+        value = {usuario.nombre}
+        onChange={(event) =>{
+          setUsuario({
+            ...usuario,
+            nombre: event.target.value,
+          })
+        }}
+        />
+      <h1>Apellido: {usuario.apellidos}</h1>
+      <input
+        value = {usuario.apellidos}
+        onChange={(event) =>{
+          setUsuario({
+            ...usuario,
+            apellidos: event.target.value,
+          })
+        }}
+        />
     </>
   );
 };
+
